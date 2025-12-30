258fee46e407676ecdce93b1f9930f22cbee4c25
hakumai22 <coro501wa@outlook.jp>
2025-12-30 15:19:34 +0900

    あ

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 const app = express();
-const port = 3001;
+const port = Number(process.env.PORT ?? 3001);
 
 // Allow express to parse JSON bodies
 app.use(express.json());
@@ -18,7 +18,7 @@ app.post("/api/token", async (req, res) => {
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
-      client_id: process.env.VITE_DISCORD_CLIENT_ID,
+      client_id: process.env.DISCORD_CLIENT_ID ?? process.env.VITE_DISCORD_CLIENT_ID,
       client_secret: process.env.DISCORD_CLIENT_SECRET,
       grant_type: "authorization_code",
       code: req.body.code,
